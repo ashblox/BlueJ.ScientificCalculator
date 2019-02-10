a8d1e11d72c0e6e8965a6f496e99a93af8f9b975
yang shu <yangtree21st@gmail.com>
2019-02-10 13:41:04 -0500

    upload the calculator file

---
+++ Calculator.java
@@ -0,0 +1,56 @@
+
+/**
+ * Write a description of class Calculator here.
+ *
+ * @author (Yangtree21st)
+ * @version (Feb.10.2019)
+ */
+
+    // instance variables - replace the example below with your own
+public class Calculator
+{
+    public static double multiply(double fNum, double sNum)
+    {
+        return fNum * sNum;
+    }
+    
+    public static double divide(double fNum, double sNum)
+    {
+        return fNum / sNum;
+    }
+    
+    public static double add(double fNum, double sNum)
+    {
+        return fNum  + sNum;
+    }
+    
+    public static double subtract(double fNum , double sNum)
+    {
+        return fNum  - sNum;
+    }
+    
+    public static double square(double fNum )
+    {
+        return fNum  * fNum;
+    }
+    
+    public static double sqrt(double fNum )
+    {
+        return Math.sqrt(fNum );
+    }
+    
+    public static double pow(double fNum , double sNum)
+    {
+        return Math.pow(fNum , sNum);
+    }
+    
+    public static double inverse(double fNum )
+    {
+        return 1 / fNum ;
+    }
+    
+    public static double changeSign(double fNum )
+    {
+        return -fNum ;
+    }
+}
